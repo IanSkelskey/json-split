ba3e941f6e783126dff11c2b244f760bfe2c813d
Ian Skelskey <46094112+IanSkelskey@users.noreply.github.com>
2024-05-24 13:05:05 -0400

    Updated main function to use InquirerPy for interactive prompts when arguments are not provided.

--- src/json_split.py
+++ src/json_split.py
@@ -28,13 +28,37 @@ def split_json(input_file, output_dir, num_files):
 
     print(Fore.GREEN + 'Split {} into {} files in {}'.format(input_file, num_files, output_dir))
 				end = num_items
-			output_file = os.path.join(output_dir, 'output_{}.json'.format(i))
-			with open(output_file, 'w') as f:
-				json.dump(data[start:end], f, indent=4)
-    
-	print('Split {} into {} files in {}'.format(input_file, num_files, output_dir))
- 
-if __name__ == '__main__':
+def main():
+    if len(sys.argv) != 4:
+        questions = [
+            {
+                "type": "input",
+                "message": "Please enter the input JSON file:",
+                "name": "input_file",
+            },
+            {
+                "type": "input",
+                "message": "Please enter the output directory (default: output):",
+                "name": "output_dir",
+                "default": "output",
+            },
+            {
+                "type": "input",
+                "message": "Please enter the number of files to split into (default: 10):",
+                "name": "num_files",
+                "default": "10",
+            }
+        ]
+        answers = prompt(questions)
+        input_file = answers["input_file"]
+        output_dir = answers["output_dir"]
+        num_files = int(answers["num_files"])
+    else:
+        input_file = sys.argv[1]
+        output_dir = sys.argv[2]
+        num_files = int(sys.argv[3])
+
+    split_json(input_file, output_dir, num_files)
 	if len(sys.argv) != 4:
 		print('Usage: python json_split.py <input_file> <output_dir> <num_files>')
 		sys.exit(1)
